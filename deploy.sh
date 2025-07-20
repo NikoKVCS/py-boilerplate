b7853585686cb069f9c0c0a913fb0f6e5fa01060
nikokvcs <kvcsgame@hotmail.com>
2025-07-20 05:06:11 +0000

    feat: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+# 定义项目名
+PROJECT_NAME="py_boilerplate"
+
 git pull
 
-docker stop fnagentcontainer
-docker rm fnagentcontainer
+docker stop ${PROJECT_NAME}_container
+docker rm ${PROJECT_NAME}_container
 
-docker build -t fnagentimage .
-docker run -d -p 8000:8000 --name fnagentcontainer --restart always fnagentimage
+docker build -t ${PROJECT_NAME}_image .
+docker run -d -p 8000:8000 --name ${PROJECT_NAME}_container --restart always ${PROJECT_NAME}_image
